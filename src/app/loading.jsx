073ea09102a9c2246a4e093c9e4bd6802c16752d
Pouya <zarepouya7@gmail.com>
2023-03-07 12:48:25 +0200

    adding spinner to loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='flex justify-center'>
+        <img className='h-96' src='loading.svg' width={100} height={100}/>
+    </div>
+  )
+}
+
+export default loading
