d6bd5238d0ef468cb9ce5454588e31b8f9c1e801
Pouya <zarepouya7@gmail.com>
2023-03-07 13:35:02 +0200

    add card to the webpage

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card({result}) {
+  return (
+    <div>{result.title}</div>
+  )
+}
+
+export default Card
--- src/app/components/Results.jsx
+++ src/app/components/Results.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Card from './Card'
 
 function Results({results}) {
   return (
     <div>
         {results?.map((result) => (
-            <div className='text-white' key={result.id}>
-                {result.title}
+            <div className='sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 max-w-6xl mx-auto py-4 text-white' key={result.id}>
+                <Card result={result}/>
             </div>
         ))}
     </div>
