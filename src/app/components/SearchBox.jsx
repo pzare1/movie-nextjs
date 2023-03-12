5c73caa5b43ba37afec2074f069eb31f53823206
Pouya <zarepouya7@gmail.com>
2023-03-13 01:32:29 +0200

    add search page component

--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import {BiSearch} from 'react-icons/bi'
+"use client"
+import React, { useState } from 'react';
+import { BiSearch } from 'react-icons/bi';
+import { useRouter } from "next/navigation";
+
 function SearchBox() {
+  const [search, setSearch] = useState('');
+  const router = useRouter();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/search/${search}`);
+  }
   return (
-    <div className="relative">
-    <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
-      <BiSearch className="text-gray-400" />
-    </div>
-    <input
-      type="text"
-      placeholder="Search..."
-      className="pl-8 pr-2 py-2 border border-slate-500 rounded-md bg-transparent focus:outline-none focus:border-cyan-500 flex items-center"
-    />
-  </div>
+    <form className="relative flex justify-between" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="pl-4 pr-2 py-2 placeholder-gray-600 border border-gray-600 rounded-tr-none rounded-br-none rounded-md bg-transparent focus:outline-none focus:border-cyan-500 flex items-center"
+      />
+      <button disabled={!search} className='bg-cyan-500 p-2 rounded-tr-md rounded-br-md'><BiSearch className="text-white" /></button>
+    </form>
   );
 }
 
-export default SearchBox
+export default SearchBox;
---
+++ src/app/search/[searchTerm]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-white'>slsjdnsjds</div>
+  )
+}
+
+export default page
